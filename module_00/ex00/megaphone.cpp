d4c611133a5c3a60f47c7ca5840254ecc5dbd88d
Касим Байбиков <kasim.baybikov@gmail.com>
2021-06-15 01:04:21 +0300

    first part

---
+++ module_00/ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+#include <cctype>
+#include <iostream>
+#include <ostream>
+
+int main(int ac, char **av)
+{
+	if (ac <= 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	int i = 0;
+	while (av[++i])
+	{
+		int j = -1;
+		while (av[i][++j])
+		{
+			std::cout << (char)toupper(av[i][j]);
+		}
+		if (i != ac -1)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+	return (0);
+}
