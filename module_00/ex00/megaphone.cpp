c44312c2b58036163a60cd1f1105e6aa8d42bfae
IHoneyBunnyI <kasim.baybikov@gmail.com>
2021-06-28 18:22:51 +0400

    fix tab

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -4,7 +4,7 @@ int main(int ac, char **av)
 {
 	if (ac <= 1)
 	{
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
 	int i = 0;
