0a459c2f283191c21c3c12f498dee1abca9191a2
Касим Байбиков <kasim.baybikov@gmail.com>
2021-07-05 00:44:27 +0300

    start ex02

---
+++ module_00/ex02/Account.cpp
@@ -0,0 +1,30 @@
+#include "Account.hpp"
+#include <iostream>
+
+int Account::getNbAccounts()
+{
+	return (_nbAccounts);
+}
+
+int Account::getTotalAmount()
+{
+	return (_totalAmount);
+}
+
+int Account::getNbDeposits()
+{
+	return (_totalNbDeposits);
+}
+
+int Account::getNbWithdrawals()
+{
+	return (_totalNbWithdrawals);
+}
+
+void Account::displayAccountsInfos()
+{
+	std::cout << "accounts:" << _nbAccounts << ";";
+	std::cout << "total:" << _totalAmount << ";";
+	std::cout << "deposits:" << _totalNbDeposits << ";";
+	std::cout << "withdrawals:" << _totalNbWithdrawals << std::endl;
+}
