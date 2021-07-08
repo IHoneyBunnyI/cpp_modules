b892e94aefb12b2500b6df5e21040138ee1ee1c7
Касим Байбиков <kasim.baybikov@gmail.com>
2021-07-08 14:44:12 +0300

    fix bug ex00, bruh

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -2,12 +2,13 @@
 
 int main(int ac, char **av)
 {
+	int i = 0;
+
 	if (ac <= 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	int i = 0;
 	while (av[++i])
 	{
 		int j = -1;
@@ -15,8 +16,6 @@ int main(int ac, char **av)
 		{
 			std::cout << (char)toupper(av[i][j]);
 		}
-		if (i != ac - 1)
-			std::cout << " ";
 	}
 	std::cout << std::endl;
 	return (0);
