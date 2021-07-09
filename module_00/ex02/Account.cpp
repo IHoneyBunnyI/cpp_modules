fe14dceed1015d7f993e1faff16ebed8d4bf28b7
Касим Байбиков <kasim.baybikov@gmail.com>
2021-07-09 10:58:26 +0300

    ex02

--- module_00/ex02/Account.cpp
+++ module_00/ex02/Account.cpp
@@ -1,5 +1,54 @@
 #include "Account.hpp"
 #include <iostream>
+#include <chrono>
+#include <iomanip>
+#include <ostream>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+{
+	this->_accountIndex = this->_nbAccounts++;
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	this->_totalAmount += this->_amount;
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "created" << std::endl;
+}
+
+Account::~Account()
+{
+	this->_nbAccounts--;
+	this->_totalAmount -= this->_amount;
+	this->_totalNbWithdrawals -= this->_nbWithdrawals;
+	this->_totalNbDeposits -= this->_nbDeposits;
+
+	this->_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "closed" << std::endl;
+}
+
+int	Account::checkAmount( void ) const
+{
+	return (this->_amount);
+}
+
+void	Account::displayStatus( void ) const
+{
+	this->_displayTimestamp();
+
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "deposits:" << this->_nbDeposits << ";";
+	std::cout << "withdrawals:" << this->_nbWithdrawals << std::endl;
+}
 
 int Account::getNbAccounts()
 {
@@ -27,4 +76,25 @@ void Account::displayAccountsInfos()
 	std::cout << "total:" << _totalAmount << ";";
 	std::cout << "deposits:" << _totalNbDeposits << ";";
 	std::cout << "withdrawals:" << _totalNbWithdrawals << std::endl;
+}
+
+void Account::_displayTimestamp()
+{
+	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+	time_t	tt = std::chrono::system_clock::to_time_t(now);
+	tm local_tm = *localtime(&tt);
+
+	std::cout << std::put_time(&local_tm, "[%Y%m%d_%H%M%S] ");
+}
+
+void Account::makeDeposit(int deposit)
+{
+	;
+}
+
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	this->_displayTimestamp();
+	return true;
 }
