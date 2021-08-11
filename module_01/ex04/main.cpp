15cd9c779cc90d5d6a70f91c24ad770b61ef6b3a
IHoneyBunnyI <kasim.baybikov@gmail.com>
2021-08-11 19:31:13 +0300

    ex04

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -2,6 +2,11 @@
 
 int main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
+	if (ac != 4){
+		std::cout << "Error" << std::endl;
+		return (0);
+	}
+	std::string s1(av[2]);
+	std::string s2(av[3]);
+	std::cout << s1 << s2;
 }
