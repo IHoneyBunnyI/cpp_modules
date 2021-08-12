cf017bc2e31a0e8e3f70f07e6d666771a6c753ef
IHoneyBunnyI <kasim.baybikov@gmail.com>
2021-08-12 14:07:10 +0300

    ex04 ok

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -4,11 +4,22 @@
 #include <fstream>
 #include <string>
 
-std::string ft_replace(std::string line, std::string s1, std::string s2)
+std::string replaceAll( std::string const& original, std::string const& from, std::string const& to )
 {
-	(void)s1;
-	(void)s2;
-	return line;
+	std::string new_line;
+	std::string results;
+	std::string::const_iterator end = original.end();
+	std::string::const_iterator current = original.begin();
+	std::string::const_iterator next = std::search( current, end, from.begin(), from.end() );
+	while ( next != end )
+	{
+		results.append( current, next );
+		results.append( to );
+		current = next + from.size();
+		next = std::search( current, end, from.begin(), from.end() );
+	}
+	results.append( current, next );
+	return results;
 }
 
 std::string upper(std::string str)
@@ -17,7 +28,6 @@ std::string upper(std::string str)
 	for (size_t i = 0; i < str.length(); i++)
 		new_str += std::toupper(str[i]);
 	return new_str;
-
 }
 
 int main(int ac, char **av)
@@ -34,7 +44,8 @@ int main(int ac, char **av)
 	{
 		std::string line;
 		std::getline(file_in, line);
-		std::string new_line = ft_replace(line, s1, s2);
-		file_out << new_line << std::endl;
+		line = replaceAll(line, s1, s2);
+		if (!file_in.eof())
+			file_out << line << std::endl;
 	}
 }
