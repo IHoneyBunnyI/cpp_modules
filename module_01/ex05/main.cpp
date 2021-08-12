5be101219b965f08157af5ab5a28fc9b83c1480c
IHoneyBunnyI <kasim.baybikov@gmail.com>
2021-08-12 19:09:29 +0300

    ex05 ok

---
+++ module_01/ex05/main.cpp
@@ -0,0 +1,12 @@
+#include "Karen.hpp"
+
+int main()
+{
+	Karen(karen);
+	karen.complain("INFO");
+	karen.complain("DEBUG");
+	karen.complain("WARNING");
+	karen.complain("ERROR");
+	karen.complain("aaa");
+	karen.complain("LOL");
+}
