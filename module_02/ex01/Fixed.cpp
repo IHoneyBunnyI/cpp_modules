f8367ea0a895a093109ed8cc68463e5759afe2da
IHoneyBunnyI <kasim.baybikov@gmail.com>
2021-08-13 17:00:05 +0300

    ex01 ok

--- module_02/ex01/Fixed.cpp
+++ module_02/ex01/Fixed.cpp
@@ -10,6 +10,18 @@ Fixed::Fixed()
 	std::cout << "Default constructor called" << std::endl;
 }
 
+Fixed::Fixed(const int value)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->value = value * (1 << Fixed::number_of_fractional_bits);
+}
+
+Fixed::Fixed(const float value)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->value = (int)roundf(value * (1 << Fixed::number_of_fractional_bits));
+}
+
 Fixed::~Fixed()
 {
 	std::cout << "Destructor called" << std::endl;
@@ -37,4 +49,20 @@ int Fixed::getRawBits() const
 void Fixed::setRawBits(const int raw)
 {
 	this->value = raw;
+}
+
+int Fixed::toInt() const
+{
+	return (this->value >> Fixed::number_of_fractional_bits);
+}
+
+float Fixed::toFloat( void ) const
+{
+	return ((float)this->value / (float)(1 << Fixed::number_of_fractional_bits));	
+}
+
+std::ostream& operator << (std::ostream& cout, const Fixed& fixed)
+{
+	cout << fixed.toFloat();
+	return (cout);
 }
--- module_02/ex01/Fixed.hpp
+++ module_02/ex01/Fixed.hpp
@@ -1,6 +1,8 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+#include <iostream>
+
 class Fixed
 {
 private:
@@ -8,12 +10,19 @@ private:
 	static const int number_of_fractional_bits;
 public:
 	Fixed();
-	~Fixed();
+	Fixed(const int value);
+	Fixed(const float value);
 	Fixed(const Fixed& fixed);
+	~Fixed();
+
 	Fixed &operator = (const Fixed& fixed);
+
 	int getRawBits() const;
 	void setRawBits(int const raw);
-
+	float toFloat( void ) const;
+	int toInt( void ) const;
 };
+
+std::ostream& operator << (std::ostream& cout, const Fixed& fixed);
 
 #endif
