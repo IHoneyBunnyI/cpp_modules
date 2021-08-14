7a52571cd2b83ea7fa674bdd29403181f3b86dfb
IHoneyBunnyI <kasim.baybikov@gmail.com>
2021-08-14 14:17:19 +0300

    ex00 start

---
+++ module_03/ex00/ClapTrap.hpp
@@ -0,0 +1,15 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP 
+
+class ClapTrap
+{
+private:
+
+public:
+		ClapTrap();
+		~ClapTrap();
+		ClapTrap(const ClapTrap& claptrap);
+		ClapTrap &operator = (const ClapTrap& claptrap);
+};
+
+#endif
