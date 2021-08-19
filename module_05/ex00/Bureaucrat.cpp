75c05ccda5a79798210f3c0afe2d293b92e77ab5
IHoneyBunnyI <kasim.baybikov@gmail.com>
2021-08-19 17:25:09 +0300

    ex00 start

--- module_05/ex00/Bureaucrat.cpp
+++ module_05/ex00/Bureaucrat.cpp
@@ -1,3 +1,48 @@
 #include "Bureaucrat.hpp"
 
+Bureaucrat::Bureaucrat(): name("NoName"), grade(150){}
 
+Bureaucrat::~Bureaucrat() {}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& ref)
+{
+	this->name = ref.name;
+	this->grade = ref.grade;
+}
+
+Bureaucrat& Bureaucrat::operator = (const Bureaucrat& ref)
+{
+	this->name = ref.name;
+	this->grade = ref.grade;
+	return (*this);
+}
+
+
+std::string Bureaucrat::getName() const
+{
+	return (this->name);
+}
+
+int Bureaucrat::getGrade() const
+{
+	return (this->grade);
+}
+
+void Bureaucrat::incrementGrade()
+{
+	if (this->grade == 1)
+		return;
+	this->grade--;
+}
+void Bureaucrat::decrementGrade()
+{
+	if (this->grade == 150)
+		return;
+	this->grade++;
+}
+
+std::ostream& operator << (std::ostream& cout, const Bureaucrat& ref)
+{
+	cout << ref.getName() << ", bureaucrat grade " << ref.getGrade() << std::endl;
+	return (cout);
+}
--- module_05/ex00/Bureaucrat.hpp
+++ module_05/ex00/Bureaucrat.hpp
@@ -1,15 +1,27 @@
 #ifndef BUREAUCRAT_HPP
 #define BUREAUCRAT_HPP
 
+#include <iostream>
+
 class Bureaucrat
 {
 	private:
+		std::string name;
+		int grade;
 
 	public:
 		Bureaucrat();
 		~Bureaucrat();
 		Bureaucrat(const Bureaucrat& ref);
 		Bureaucrat& operator = (const Bureaucrat& ref);
+
+
+		std::string getName() const;
+		int getGrade() const;
+		void incrementGrade();
+		void decrementGrade();
 };
+
+std::ostream& operator << (std::ostream& cout, const Bureaucrat& ref);
 
 #endif
